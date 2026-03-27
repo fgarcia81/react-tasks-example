740010328a8efd6b9420822e7ff85becd6e199c0
FidelG <fmgp20@hotmail.com>
2026-03-27 01:09:21 -0600

    Updates

---
+++ data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+{
+    id:0,
+    title:"mi tarea cero",
+    description:"mi tarea cero"
+},
+{
+    id:1,
+    title:"mi primer tarea",
+    description:"mi primer tarea"
+},
+{
+    id:2,
+    title:"mi tercer tarea",
+    description:"mi tercer tarea"
+}
+]
